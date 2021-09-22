4d5af83aa7422077ed8738fe867d052c0cb08496
unknown <anguyen@softechiq.com>
2021-09-22 09:54:07 -0700

    Add count with usestate hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-
+import { useState } from 'react';
 
 function App() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
-            <h2> Next.js </h2>
+            <h2>Next.js</h2>
+            <button onClick={increment}> {count} </button>
         </div>
     )
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
